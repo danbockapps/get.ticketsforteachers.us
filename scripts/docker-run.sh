cf9381d0d212e421e3a37d4c949d9b104a56130d
Dan Bock <danbock@gmail.com>
2026-04-26 21:02:12 -0400

    Update path

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,6 +1,6 @@
   docker run -d \
     -p 3000:3000 \
-    -v /path/on/vps/data:/app/data \
+    -v /var/lib/tickets-for-teachers:/app/data \
     --env DATABASE_PATH=/app/data/database.db \
     --name tickets-for-teachers \
     tickets-for-teachers
